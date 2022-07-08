eee85bc08d462ede1847806e2fde43b18642a677
Ezra <gerkibz@outlook.com>
2022-07-08 15:46:51 +0300

    add if massage is pay response

--- reply.js
+++ reply.js
@@ -1,11 +1,23 @@
-const replyMessage = async (message) =>{
+const { makePayment } = require("./helpers")
+
+const replyMessage = async (body) =>{
     let response = ""
     let regex = /[A-Z]{3}\s\d{3}\S$/g
         try {
-            if (message ==="Hi"){
+            if (message =="Hi"){
                 response= "Please enter your Number Plate"
-            }else if (regex(message)){
-                response= "Your amout is "
+            }else if (regex(body.text.body)){
+                response= "Your amout is 300"
+            }
+            else if (message == "Pay"){
+                makePayment(
+                    {
+                        "Amount": 300,
+                        "PhoneNumber": body.from,
+                        "NotificationCallBackUrl": "http://5f6a-41-139-133-17.ngrok.io/payment-callback ",
+                        "AccountReference": "Test" 
+                    }
+                )
             }
         }catch(e){
             console.log(e)
