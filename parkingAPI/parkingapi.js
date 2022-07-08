85e5ce6ce8763064fd88df41a3787cf0a51a3d55
amschell <100097727+amschell@users.noreply.github.com>
2022-07-08 13:32:57 +0300

    added parkingApi

--- parkingAPI/parkingapi.js
+++ parkingAPI/parkingapi.js
@@ -1,6 +1,24 @@
-const checkPlate = (plate_number) => {
+const vehicleModel= require("../Models/vehicle")
+
+const checkPlate = async  (req, res) => {
+try{
+
+    const {plateNo}=req.body
+ const plate=   vehicleModel.findOne({
+plateNo:plateNo
+ })
+ if(!plateExists){
+    res.status(404).json({message:"plate number does not exist"})
+ }
+ res.status(204).json(plate)
+}
+catch(error){
+console.error(error)
+
+}
     
 }
+
 
 
 module.exports = {
