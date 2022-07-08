eccaa172408c4b9cc1422062ecf1072d7aaee0c1
amschell <100097727+amschell@users.noreply.github.com>
2022-07-08 14:04:09 +0300

    added models

---
+++ Models/vehicle.js
@@ -0,0 +1,33 @@
+const mongoose= require("mongoose")
+const vehicleSchema= new mongoose.Schema(
+
+{
+    plateNo:
+    {
+        type:String,
+        required:true,
+        trim:true
+    },
+     duration:
+    {
+        type:Number,
+        required:true,
+        trim:true,
+
+    },
+    amount:
+    {
+        type:Number,
+        required:true,
+        trim:true,
+
+    }
+
+
+
+}
+
+
+)
+
+module.exports= mongoose.model("Vehicles",vehicleSchema )
